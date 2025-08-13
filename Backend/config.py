098d5be67c90eab5409f0cbb10cbf3c9807e83d3
Kevin Huang <kshk861207@gmail.com>
2025-08-14 05:25:21 +0800

    簡單給別人使用docker測試的本地版本

--- Backend/config.py
+++ Backend/config.py
@@ -1,7 +1,9 @@
-admin = '1'
-expire_time_access = 60 #min
-# expire_time_refresh = 10 #min
-guest = '2'
-guest_mode = True
+admin = '1'  #管理者id(預設為第一個被創建的帳號就是管理者)
+expire_time_access = 60 #min (設定token過期時間，預設60分鐘後過期)
+guest = '2'  #訪客id(預設第二個被創立的帳號只能看不能編輯)
+guest_mode = True #訪客模式開關(設False即會取消訪客不能編輯的限制)
+
+
+#管理者額外路由見Backend>resources>user.py
 
 
---
+++ Readme.md
@@ -0,0 +1,11 @@
+本專案目前僅供本地開發，尚未能夠線上部屬
+後續將進行:資料庫容器化>線上部屬
+
+
+
+開發者起始步驟:
+    1. 將.env.example改成.env，並在裡面按指示輸入必須的環境變數
+    2. (選擇性):由Backend>config.py可以自訂權限
+    3. (選擇性):如要自己進行docker多次運行，請將.gitignore中的data.db移除，以免資料每次都遺失
+    4. 程式運行方法: 於目標資料夾的終端bash輸入: docker-compose up --build
+    5. 前往本地網站: http://localhost:5173/?#
