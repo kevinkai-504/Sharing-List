e0613ea662fce92095d4415a1f68e6e66a08a0cd
rest-computer <kshk861207@gmail.com>
2025-09-09 21:28:31 +0800

    測試中:得到一個成功的pytest檔

--- Backend/tests/learn/test_learn.py
+++ Backend/tests/learn/test_learn.py
@@ -3,5 +3,5 @@ import pytest
 
 def test_login():
     payload = {"username":"Kevin", "password":"test"}
-    response = requests.post("http://127.0.0.1:5000", data=payload)
+    response = requests.post("http://127.0.0.1:5000/login", json=payload)
     assert response.ok 
