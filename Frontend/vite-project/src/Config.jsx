a0e101ac73006e9ed12008cb26abbb58832fb6e9
rest-computer <kshk861207@gmail.com>
2025-09-09 18:05:46 +0800

    新增pytest相關module

--- Frontend/vite-project/src/Config.jsx
+++ Frontend/vite-project/src/Config.jsx
@@ -3,6 +3,6 @@ export const Data_Format = {
     "learn": ["name", "note", "status", "build_time"],
     "learnFtag": ["name", "note", "status", "build_time"],
     "tag":["id", "name"],
-    "url_default":"https://sharing-list.onrender.com"
+    "url_default":"http://127.0.0.1:5000"
 }
 export default Data_Format
