104f54c607b24058ec4a2d7552aba74b11345b0f
rest-computer <kshk861207@gmail.com>
2025-09-09 21:05:28 +0800

    測試中:新增簡單pytest

--- Backend/tests/learn/test_learn.py
+++ Backend/tests/learn/test_learn.py
@@ -1,3 +1,7 @@
 import requests
 import pytest
 
+def test_login():
+    payload = {"username":"Kevin", "password":"test"}
+    response = requests.post("http://127.0.0.1:5000", data=payload)
+    assert response.ok 
