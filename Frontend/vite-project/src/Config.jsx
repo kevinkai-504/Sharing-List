68b0f14e560d497445b853196db1413482aa4ef5
Kevin Huang <kshk861207@gmail.com>
2025-09-29 22:32:14 +0800

    架構問題待解決:想CICD的話初始帳號必須自動設立；改善前端環境變數問題，之後應可透過CICD直接注入變數

--- Frontend/vite-project/src/Config.jsx
+++ Frontend/vite-project/src/Config.jsx
@@ -1,8 +1,9 @@
+const VITE_API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 export const Data_Format = {
     "learn_allowChange": ["name", "status", "note"],
     "learn": ["name", "note", "status", "build_time"],
     "learnFtag": ["name", "note", "status", "build_time"],
     "tag":["id", "name"],
-    "url_default":"http://127.0.0.1:5000"
+    "url_default":VITE_API_URL
 }
 export default Data_Format
